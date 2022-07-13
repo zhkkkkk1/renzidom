60c4eb5472bdb7adfcd4241f49940e97573aa771
zhk <1219192779@qq.com>
2022-07-13 11:52:02 +0800

    初始化项目

---
+++ src/settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  title: 'nnnnnnnnnnnn后台',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   * 设置appMain右侧主题上面的header是不是固定定位
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   * 设置侧边栏上面的logo是否显示
+   */
+  sidebarLogo: true
+}
---
+++ src/store/modules/user.js
@@ -0,0 +1,8 @@
+const state = {}
+const mutations = {}
+const actions = {}
+
+export default {
+  namespaced: true,
+  state, mutations, actions
+}
